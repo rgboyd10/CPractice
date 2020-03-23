43f3d9cc7345f303cf2626c340364cd5b631b839
Russell <russell.boyd@romeoeng.com>
2020-03-22 22:55:23 -0500

    -

---
+++ programming_in_c/ex2.4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 87 - 15;
+	printf("87 - 15 = %i.\n",x);
+	return 0;
+}
---
+++ programming_in_c/ex2.5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int sum;
+	//COMPUTE RESULT
+	sum = 25 + 37 - 19;
+	
+	//DISPLAY RESULT
+	printf("The answer is %i\n", sum);
+	
+	return 0;
+}
