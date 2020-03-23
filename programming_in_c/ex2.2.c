be91cf57b4c2c05218835f16bb27bb2de2092be7
Russell Boyd <rgboyd10@gmail.com>
2020-03-22 20:48:58 -0500

    -

---
+++ programming_in_c/ex2.2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("1. In C, lowercase letters are significant.\n2. main() is where the program begins.\n3. Opening and closing braces enclose a program statements in a routine.\n4. All program statements must be terminated by a semicolon.\n");
+
+	return 0;
+}
