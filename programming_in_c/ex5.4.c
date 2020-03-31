784cc1aa2790b8dd00c21c771a65b705548fe8a1
Russell <russell.boyd@romeoeng.com>
2020-03-31 12:49:30 -0500

    -

--- programming_in_c/ex5.4.c
+++ programming_in_c/ex5.4.c
@@ -5,9 +5,40 @@
 
 int main(void)
 {
-	float num;
+	float num, result;
 	char op;
+
 	printf("Begin Calculations\n");
-	scanf("%f %c", &num, &op);
+
+	while(op != 'E')
+	{
+		scanf("%f %c", &num, &op);
+	
+		if(op == 'S'){
+			printf("	Set Accumulator to %f\n",num);
+			result = num;
+		}
+		else if(op == '/'){
+			printf("	Divide by %f\n", num);
+			result /= num;
+		}
+		else if(op == '-'){
+			printf("	Subtract %f\n", num);
+			result -= num;
+		}
+		else if(op == '*'){
+			printf("	Multiple by %f\n", num);
+			result *= num;
+		}
+		else if(op == '+'){
+			printf("	Add by %f\n", num);
+			result += num;
+		}
+		
+		printf("= %f	Contents of Accumulator\n", result);
+	}
+	
+	printf("= %f\nEnd of Calculations.\n", result);
+
 	return 0;	 
 }
