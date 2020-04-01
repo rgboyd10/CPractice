3735c4281442194e1f954dd896b239a534d291ad
Russell <russell.boyd@romeoeng.com>
2020-04-01 17:44:32 -0500

    -

---
+++ programming_in_c/ex6.1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int values [10];
+	int sz = sizeof values / sizeof values[0];
+	int index;
+	
+	for(int x = 0; x < sz; x++){
+		values[x] = 0;
+	}
+
+	values[0] = 197;
+	values[2] = -100;
+	values[5] = 350;
+	values[3] = values[0] + values[5];
+	values[9] = values[5] / 10;
+	--values[2];
+	
+	for(index = 0; index < sz; index++)
+	{
+		printf("values[%i] = %i\n", index, values[index]);
+	}	
+	
+	return 0;
+}
