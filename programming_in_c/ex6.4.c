0ec3e1f706d0fa200e7e7e9b3affff80ec9f29ab
Russell Boyd <rgboyd10@gmail.com>
2020-04-03 15:01:48 -0500

    -

--- programming_in_c/ex6.4.c
+++ programming_in_c/ex6.4.c
@@ -4,17 +4,21 @@
 int main(void)
 {
 	float floop[10];
-	float val, total = 0.0;
-	int x = 1;
-	while(x < 10)
-	{
-		printf("Enter in %i / 10 values for an array.",x);
-		scanf("%f\n", &val);
-		floop[x] = val;
-		printf("Array value: %f\n",floop[x]); 
-		total += val;
-		x++;	
+	float val, total;
+	int floop_size = sizeof floop / sizeof floop[0];
+
+	for(int x = 0; x < floop_size; x++)
+	{	
+		printf("Enter in %i / 10 values for an array.\n",x + 1);
+		scanf("%f",&floop[x]);
+		total += floop[x];
 	}
-	printf("Average array values: %f\n",total / 10.0);
+	
+	for(int y = 0; y < floop_size; y++)
+		printf("Array value %i: %f\n",y + 1,floop[y]);
+
+	printf("Floop size: %i\n",floop_size);
+	printf("Average array values: %f\n",total / floop_size);
+
 	return 0; 
 }
