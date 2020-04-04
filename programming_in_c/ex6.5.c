753f9198a4f600de1100aa73df52421d069d774e
Russell Boyd <rgboyd10@gmail.com>
2020-04-04 00:13:27 -0500

    -

--- programming_in_c/ex6.5.c
+++ programming_in_c/ex6.5.c
@@ -6,13 +6,13 @@ int main(void)
 	int i, j;
 
 	for(j = 0; j < 10; ++j)
-		for(i = 0 i < j; ++i)
+		for(i = 0; i < j; ++i)
 			numbers[j] += numbers[i];
 			//numbers[j] = numbers[j] + numbers[i]
 			
 	for(j = 0; j < 10; ++j)
 		printf("%i", numbers[j]);
-			/*output of 1 1*/
+			/*output of 1 1 2 4...*/
 	printf("\n");
 
 	return 0;
