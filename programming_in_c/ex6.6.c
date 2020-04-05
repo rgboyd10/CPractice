48cf3c2d2d4eddbb7e027813f6a4f683e5bc2aad
Russell Boyd <rgboyd10@gmail.com>
2020-04-05 17:02:33 -0500

    -

--- programming_in_c/ex6.6.c
+++ programming_in_c/ex6.6.c
@@ -1,12 +1,17 @@
 /*A program to generate the first 15 Fibonacci numbers revised*/
 #include <stdio.h>
 
-int main(void)i
+int main(void)
 {
-	int oneprev, twoprev, current;
-	
+	int oneprev = 1, twoprev = 0, current = twoprev + oneprev;
+	printf("%i, %i, ", twoprev, oneprev);
 	for(int x = 1; x <= 15; x++)
 	{
-		
+		/*0, 1, 1, 2, 3, 5...*/
+		printf("%i, ", current);
+		current += oneprev;
+		oneprev = current - oneprev;
 	}	
+	printf("\n");
+	return 0;
 }
