05d67ea39309078aff7a31da8daf21d8f0555ca7
Russell Boyd <rgboyd10@gmail.com>
2020-04-05 01:56:29 -0500

    -

---
+++ programming_in_c/ex6.6.c
@@ -0,0 +1,12 @@
+/*A program to generate the first 15 Fibonacci numbers revised*/
+#include <stdio.h>
+
+int main(void)i
+{
+	int oneprev, twoprev, current;
+	
+	for(int x = 1; x <= 15; x++)
+	{
+		
+	}	
+}
