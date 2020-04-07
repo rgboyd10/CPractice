4419ae41ba928778742daa78fa6dfdf432989781
Russell <russell.boyd@romeoeng.com>
2020-04-07 15:01:53 -0500

    -

--- programming_in_c/ex7.4.c
+++ programming_in_c/ex7.4.c
@@ -17,9 +17,10 @@ float squareRoot(float x)
 	float guess = 1.0;
 	
 	while(absoluteValue (guess * guess - x) >= epsilon)
-		printf("%f\n", guess);
-		guess = (x / guess + guess) / 2.0;
-	
+		{	
+			guess = (x / guess + guess) / 2.0;
+			printf("%f\n", guess);
+		}
 	return guess;
 }
 
