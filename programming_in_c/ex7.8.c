b133b830cef86f332fca6bdf43264afbc73d5ce2
Russell Boyd <rgboyd10@gmail.com>
2020-04-09 15:33:58 -0500

    -

--- programming_in_c/ex7.8.c
+++ programming_in_c/ex7.8.c
@@ -1,4 +1,4 @@
-//a program to calculate the positive and negative roots using a quadratic formula.
+//a program to calculate the positive and negative roots using a quadratic formula and handle some other things.
 #include <stdio.h>
 #include <math.h>
 	
@@ -17,9 +17,14 @@ double calculate_quadratic_negative(double a, double b, double c)
 int main(void)
 {
 	double a, b, c;
+	double discriminant;
 	printf("To solve for the roots of a quadratic equation, please enter an a, b, and c value: ");
 	scanf("%lf %lf %lf", &a, &b, &c);
-	printf("The positive root is %lf and the negative root is %lf.", calculate_quadratic_positive(a, b, c), calculate_quadratic_negative(a, b, c)); 
-
+	discriminant = pow(b,2)-4*a*c;
+	if(discriminant > 0)
+	printf("The positive root is %lf and the negative root is %lf.\n", calculate_quadratic_positive(a, b, c), calculate_quadratic_negative(a, b, c)); 
+	else	
+	printf("The positive root is %lf and the negative root is %lf because they are imaginary numbers.\n", calculate_quadratic_positive(a, b, c), calculate_quadratic_negative(a, b, c)); 
+	
 	return 0;
 }
