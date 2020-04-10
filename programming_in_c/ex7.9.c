2e4990b639439ffbac47d4512e17377eab3deea8
Russell Boyd <rgboyd10@gmail.com>
2020-04-10 00:42:16 -0500

    -

--- programming_in_c/ex7.9.c
+++ programming_in_c/ex7.9.c
@@ -3,10 +3,25 @@
 
 int lcm(int u, int v)
 {
-	int result = (u*v)/ 
+	int x = 1;
+	int leastcm;
+	while(x < u * v)
+	{
+		if(u * x == v * x)
+		{
+			leastcm = x;
+			break;
+		}
+		x++;
+	}
 }
 
 int main(void)
 {
-	int result;
+	int u, v;
+	printf("Please enter two integers that you would like to know the least common multiple of:");
+	scanf("%i %i", &u, &v);
+	int result = lcm(u, v);
+	printf("The least common multiple of %i and %i is %i.\n",u,v,result);
+	return 0;
 }
