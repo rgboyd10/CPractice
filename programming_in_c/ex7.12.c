b704196c6afb0b3a1ea530682c5dd028fca558f4
Russell <russell.boyd@romeoeng.com>
2020-04-13 14:48:46 -0500

    -

--- programming_in_c/ex7.12.c
+++ programming_in_c/ex7.12.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-int** transposeMatrix()
+int** transposeMatrix(int a[4][5], int b[5][4])
 {
+	
+	return b;
 }
 
 int main(void)
 {
 	int matrix[4][5];
+	int b[5][4];
 	int row_length = sizeof(matrix) / sizeof(matrix[0]);
 	int column_length = sizeof(matrix[0])/row_length;
 
-	printf("Please enter a 4x5 matrix: ");
+	printf("Please enter numbers to be entered into a 4x5 matrix: ");
 	for(int x = 0; x < row_length; x++)
 	{
 		for(int y = 0; y < column_length; y++)
@@ -29,6 +32,30 @@ int main(void)
 				printf("\n");
 		}
 	}
+
+	int row_length = sizeof(matrix) / sizeof(matrix[0]);
+	int column_length = sizeof(matrix[0])/row_length;
+
+	printf("Please enter numbers to be entered into a 4x5 matrix: ");
+	for(int x = 0; x < row_length; x++)
+	{
+		for(int y = 0; y < column_length; y++)
+		{
+			scanf("%i",&matrix[x][y]);
+		}
+	}
+	 
+	for(int x = 0; x < row_length; x++)
+	{	
+		for(int y = 0; y < column_length; y++)
+		{
+			printf("%i",matrix[x][y]);
+			if(y == column_length-1)
+				printf("\n");
+		}
+	}
+
+	
 
 	return 0;
 }
