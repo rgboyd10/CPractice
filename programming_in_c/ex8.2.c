0ec194683a3a800473c3125b58b3113969ac3d19
russellboyd <russellboyd@protonmail.ch>
2020-05-06 07:18:17 -0500

    -

--- programming_in_c/ex8.2.c
+++ programming_in_c/ex8.2.c
@@ -10,29 +10,45 @@ struct date
 	int year;
 };
 
-void calcDays(struct date x, struct date y)
+int f(int year, int month)
 {
-	int f, g;
-	
-	if(x.month <= 2)
-		f = x.year - 1;
+	int r;
+	if(month <= 2)
+		r = year - 1;
 	else
-		f = x.year;
+		r = year;
+	return r;
+}
 
-	if(y.month <= 2)
-		g = y.month + 13;
+int g(int month)
+{
+	int r;
+	if(month <= 2)
+		r = month + 13;
 	else
-		g = y.month + 1;
- 
-	xresult = 1461 * f / 4 + 153 * f / 5 + x.day;
-	yresult = 1461 * g / 4 + 153 * g / 5 + y.day;
+		r = month + 1;
+	
+	return r;
+}
+
+int calcDiff(struct date x, struct date y)
+{
+	int r;
+	xresult = 1461 * f(x.year, x.month) / 4 + 153 * g(x.month) / 5 + x.day;
+	yresult = 1461 * f(y.year, y.month) / 4 + 153 * g(y.month) / 5 + y.day;
+	r = xresult - yresult;
+	if(r < 0)
+		r = r * -1;
+	else
+		r = r;
+
+	return r;
 }
 
 int main(void)
 {
 	struct date first, second;
-	printf("Enter 2 dates that you would like to know the number of days between. Example format = earliest date latest date = mm:dd:yy mm:dd:yy.\n");
+	printf("Enter 2 dates that you would like to know the number of days between. Example format = mm:dd:yy mm:dd:yy.\n");
 	scanf("%i:%i:%i %i:%i:%i", &first.month, &first.day, &first.year, &second.month, &second.day, &second.year); 
-	calcDays(first, second);
-	printf("\nThe time gap is %i days.\n", yresult - xresult);
+	printf("\nThe time gap is %i days.\n", calcDiff(first,second));
 }
