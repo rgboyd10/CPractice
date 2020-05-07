5d8a371bab31a714ba03245fc4a0ed874525c473
russellboyd <russellboyd@protonmail.ch>
2020-05-06 21:59:38 -0500

    -

---
+++ programming_in_c/ex8.5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <math.h>
+
+struct time
+{
+	int seconds;
+	int minutes;
+	int hours;
+};
+
+struct date
+{
+	int day;
+	int month;
+	int year;
+};
+
+struct dateAndTime
+{
+	struct date sdate;
+	struct time stime;
+};
+
+void timeUpdate()
+{
+	
+}
+
+void dateUpdate()
+{
+
+}
+
+struct dateAndTime clockKeeper(struct dateAndTime x)
+{
+	
+}
+
+int main(void)
+{
+	//record continuous input from user. Certain commands will reduce or increase the time. 
+
+	
+	
+	return 0;
+}
