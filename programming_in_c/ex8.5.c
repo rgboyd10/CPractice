b8724f5de0f4bc0649b2453f76fa8ec0bfe1cdd0
russellboyd <russellgboyd@icloud.com>
2020-05-10 11:19:19 -0500

    -

--- programming_in_c/ex8.5.c
+++ programming_in_c/ex8.5.c
@@ -38,6 +38,8 @@ struct dateAndTime clockKeeper(struct dateAndTime x)
 {
 	struct dateAndTime result;
 
+	printf("Current time is: %i:%i:%i %i:%i:%i\n Would you like to change the date or time?\nEnter 1 for date or 2 for time.\n");	
+	scanf("%i",&dortchoice);
 	if(x.sdate.hours >= 12 && x.sdate.minutes >= 0 && x.sdate.seconds >= 0)
 	{
 		dateUpdate();
@@ -61,7 +63,6 @@ int main(void)
 	int dortchoice; 
 	printf("Please enter the current date and time in the format mm:dd:yy hh:mm:ss: \n");
 	scanf("%i:%i:%i %i:%i:%i");
-	printf("Current time is: %i:%i:%i %i:%i:%i\n Would you like to change the date or time?\nEnter 1 for date or 2 for time.\n");
 	scanf("%i",&dortchoice);
 	if(dortchoice == 1)
 	{
