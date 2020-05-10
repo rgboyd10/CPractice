180f25af55f84859637290f9f087c7e4cf27570c
russellboyd <russellboyd@protonmail.ch>
2020-05-10 02:56:50 -0500

    -

--- programming_in_c/ex8.5.c
+++ programming_in_c/ex8.5.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
+
+char amorpm[] = "AM";
 
 struct time
 {
@@ -33,13 +36,47 @@ void dateUpdate()
 
 struct dateAndTime clockKeeper(struct dateAndTime x)
 {
-	
+	struct dateAndTime result;
+
+	if(x.sdate.hours >= 12 && x.sdate.minutes >= 0 && x.sdate.seconds >= 0)
+	{
+		dateUpdate();
+	}
+	else if()
+	{
+		timeUpdate();
+	}
+	else
+	{
+		
+	}	
+
+	return result;	
 }
 
 int main(void)
 {
 	//record continuous input from user. Certain commands will reduce or increase the time. 
-
+	struct dateAndTime currentd&t;
+	int dortchoice; 
+	printf("Please enter the current date and time in the format mm:dd:yy hh:mm:ss: \n");
+	scanf("%i:%i:%i %i:%i:%i");
+	printf("Current time is: %i:%i:%i %i:%i:%i\n Would you like to change the date or time?\nEnter 1 for date or 2 for time.\n");
+	scanf("%i",&dortchoice);
+	if(dortchoice == 1)
+	{
+		cur
+	}
+	else if(dortchoice == 2)
+	{
+		
+	}
+	else
+	{
+		
+	}
+	
+	
 	
 	
 	return 0;
