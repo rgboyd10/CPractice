c29d20f9c49015a189b9da162859218508da95c9
russellboyd <russellgboyd@icloud.com>
2020-05-12 23:54:03 -0500

    -

--- programming_in_c/ex8.5.c
+++ programming_in_c/ex8.5.c
@@ -6,9 +6,9 @@ char amorpm[] = "AM";
 
 struct time
 {
-	int seconds;
-	int minutes;
-	int hours;
+	int second;
+	int minute;
+	int hour;
 };
 
 struct date
@@ -37,7 +37,18 @@ void dateUpdate()
 struct dateAndTime clockKeeper(struct dateAndTime x)
 {
 	struct dateAndTime result;
-
+	int dortchoice; 
+	printf("Please enter the current date and time in the format mm:dd:yy hh:mm:ss: \n");
+	scanf("%i:%i:%i %i:%i:%i", &sdate.month, &sdate.day, &sdate.year, &stime.hour, &stime.minute, &stime.second);
+	printf("\nEnter 1 to update the date or 0 to update the time.\n");
+	scanf("%i",&dortchoice);
+	
+	if(dortchoice == 1)
+		dateUpdate();
+	else if(dortchoice == 0)
+		timeUpdate();
+	else
+	
 	printf("Current time is: %i:%i:%i %i:%i:%i\n Would you like to change the date or time?\nEnter 1 for date or 2 for time.\n");	
 	scanf("%i",&dortchoice);
 	if(x.sdate.hours >= 12 && x.sdate.minutes >= 0 && x.sdate.seconds >= 0)
@@ -58,10 +69,11 @@ struct dateAndTime clockKeeper(struct dateAndTime x)
 
 int main(void)
 {
-	//record continuous input from user. Certain commands will reduce or increase the time. 
-	struct dateAndTime currentd&t;
-	int dortchoice; 
+	
 	printf("Please enter the current date and time in the format mm:dd:yy hh:mm:ss: \n");
+	scanf("%i:%i:%i %i:%i:%i", &sdate.month, &sdate.day, &sdate.year, &stime.hour, &stime.minute, &stime.second);
+	
+	clockKeeper(currentd&t);
 	
 	return 0;
 }
