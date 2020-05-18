e4e39cea930427d2fdf2bd9686cd92af07f17ab4
russellboyd <russellgboyd@icloud.com>
2020-05-18 16:35:38 -0500

    -

--- cppinstitute/cessentials/2.1.3.8.c
+++ cppinstitute/cessentials/2.1.3.8.c
@@ -4,7 +4,7 @@ int main()
 {
 	char cee = 'c';
 	char ayy = 'a';
-	printf("Diff beetween %c and %c is : %d\n", ayy, cee, cee - ayy);
-	printf("Diff beetween %c and %c is : %d\n", cee, ayy, ayy - cee);
+	printf("Diff beetween '%c' and '%c' is : %d\n", ayy, cee, cee - ayy);
+	printf("Diff beetween '%c' and '%c' is : %d\n", cee, ayy, ayy - cee);
 	return 0;
 }
