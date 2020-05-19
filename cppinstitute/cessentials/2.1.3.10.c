b27004f664bc934be50b90371a3b6b6b7e816614
russellboyd <russellgboyd@icloud.com>
2020-05-18 20:23:52 -0500

    -

--- cppinstitute/cessentials/2.1.3.10.c
+++ cppinstitute/cessentials/2.1.3.10.c
@@ -5,8 +5,16 @@ int main()
 	char zero = '0';
 
 	for(int x = 1; x <= 10; x++)
-	{
-		printf("'%i' - '%c' is : %i\n", x, zero, zero);
+	{	
+		if(x == 10)
+		{	x = 0;
+			printf("'%i' - '%c' is : %i\n", x, zero, x);
+			x = 10;
+		}		
+		else
+		{
+			printf("'%i' - '%c' is : %i\n", x, zero, x);
+		}
 	} 
 
 	return 0;
