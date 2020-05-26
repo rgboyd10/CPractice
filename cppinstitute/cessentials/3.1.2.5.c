f6b01e94ceb0a718c67b00a2679db9dbfcff19f0
russellboyd <russellgboyd@icloud.com>
2020-05-25 21:32:38 -0500

    -

--- cppinstitute/cessentials/3.1.2.5.c
+++ cppinstitute/cessentials/3.1.2.5.c
@@ -8,17 +8,17 @@ int main(void)
 	printf("Please provide four numbers for an ip address without the periods: \n");
 	scanf("%i %i %i %i", &w, &x, &y, &z);
 	
-	if(w <= 0 || w > 255 ||
-		x <= 0 || x > 255 ||
-		y <= 0 || y > 255 ||
-		z <= 0 || z > 255)
+	if(w < 0 || w > 255 ||
+		x < 0 || x > 255 ||
+		y < 0 || y > 255 ||
+		z < 0 || z > 255)
 	{
 		printf("Incorrect IP Address.\n");
 	}
 	else
 	{
 		printf("Human-readable IP address is: %i.%i.%i.%i\n", w, x, y, z);
-		r = (w * 256 * 256 * 256) * (x * 256 * 256) * (y * 256) * (z * 1);
+		r = (w * 256 * 256 * 256) + (x * 256 * 256) + (y * 256) + (z * 1);
 		printf("IP adrress as a 32-bit number: %lu \n", r);
 	}
 	
