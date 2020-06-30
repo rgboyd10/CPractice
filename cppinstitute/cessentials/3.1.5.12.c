52e3039f802649c93afcaeaf772400fde0ab2da8
russellboyd <russellgboyd@icloud.com>
2020-06-30 02:10:22 +0100

    -

--- cppinstitute/cessentials/3.1.5.12.c
+++ cppinstitute/cessentials/3.1.5.12.c
@@ -4,23 +4,21 @@ int getInput()
 {
 	int in;
 	scanf("%i", &in);
-	if(in > 256)
-		getInput();		
-	
 	return in;
 }
 
 int main()
 {
-	int num, hnibble, lnibble, numbytes;
+	int num;
+	unsigned char ch, hnibble, lnibble;
 	printf("Please enter a number smaller than 256.\n");
 	num = getInput();
-	numbytes = num % 4;	
-	hnibble = (numbytes & 0x0F) << 4;
-	lnibble = (numbytes & 0xF0) >> 4;
+	ch = (char)num;
+	hnibble = (ch & 0x0F) << 4;
+	lnibble = (ch & 0xF0) >> 4;
 	
-	printf("H nibble: %i\n", hnibble);
-	printf("L nibble: %i\n", lnibble);
+	printf("H nibble: %u\n", hnibble);
+	printf("L nibble: %u\n", lnibble);
 	
 	return 0;
 }
