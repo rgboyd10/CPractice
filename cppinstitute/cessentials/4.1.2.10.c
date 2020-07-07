a311716fe9cf63d0208bd53d84a82e938def62c7
russellboyd <russellgboyd@icloud.com>
2020-07-06 22:17:05 -0500

    -

--- cppinstitute/cessentials/4.1.2.10.c
+++ cppinstitute/cessentials/4.1.2.10.c
@@ -3,18 +3,14 @@
 int main()
 {
 	int fibonacci[10], lastlast = 1, last = 1, next = lastlast + last;
-       	fibonacci[0] = lastlast;
-	fibonacci[1] = last;
-	printf("%i\n",lastlast);
-	printf("%i\n",last);
+ 	printf("%i\n",lastlast);
+	printf("%i\n",last);	
 	for(int x = 2; x < 10; x++)
 	{
-		fibonacci[x] = lastlast + last;
+		fibonacci[x] = next;
+		printf("%i\n", next);
 		lastlast = last;
-		last = fibonacci[x];
-		printf("%i\n", lastlast);
-		printf("%i\n", last);
-		printf("%i\n", fibonacci[x]);
+		last = next;
 	}
 	
 		return 0;
