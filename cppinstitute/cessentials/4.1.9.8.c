054c01dd7022b0118873e4e69de9dbda17e4acd0
russellboyd <russellgboyd@icloud.com>
2020-08-31 04:25:23 +0100

    -

--- cppinstitute/cessentials/4.1.9.8.c
+++ cppinstitute/cessentials/4.1.9.8.c
@@ -10,13 +10,31 @@ int main(void)
 
 	for(int x = 0; x < sizeof(ip); x++)
 	{
-		if(ip[x] = ".")
+		if(ip[x] == ".")
 		{
 			pcount++;
+		}
+		
+		if(ip[x] == '0' ||
+		   ip[x] == '1' ||
+		   ip[x] == '2' ||
+		   ip[x] == '3' ||
+		   ip[x] == '4' ||
+       		   ip[x] == '5' ||
+		   ip[x] == '6' ||
+		   ip[x] == '7' ||
+		   ip[x] == '8' ||
+		   ip[x] == '9' ||
+		  )
+		{
+			
 		}	
 	}
 	
 	if(pcount >= 3)
 		printf("Error: not a valid address.");
-
+	else
+	{
+		
+	}
 }
