4f7107991927ba2640874644446ad5cba1251299
russellboyd <russellgboyd@icloud.com>
2020-09-09 19:21:42 -0500

    -

--- cppinstitute/cessentials/4.1.9.8.c
+++ cppinstitute/cessentials/4.1.9.8.c
@@ -38,3 +38,25 @@ int main(void)
 		
 	}
 }
+
+bool isDigit(char c)
+{
+	if(c == '0' ||
+	c == '1' ||
+	c == '2' ||
+	c == '3' ||
+	c == '4' ||
+	c == '5' ||
+	c == '6' ||
+	c == '7' ||
+	c == '8' || 
+	c == '9')
+	{
+		return true;	
+	}
+	else
+	{
+		return false;
+	}
+}
+
